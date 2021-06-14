b171c883465965eeb8b8a48aca0883714c735661
yangheat <yangheat@gmail.com>
2021-06-14 23:49:53 +0900

    #2.4

---
+++ useBeforeLeave/useBeforeLeave.js
@@ -0,0 +1,18 @@
+export const useBeforeLeave = (onBefore) => {
+  const handle = (event) => {
+    const { clientY } = event;
+    if (clientY <= 0) {
+      onBefore();
+    }
+  };
+  useEffect(() => {
+    if (typeof onBefore !== "function") {
+      return;
+    }
+    document.addEventListener("mouseleave", handle);
+
+    return () => {
+      document.removeEventListener("mouseleave", handle);
+    };
+  }, []);
+};
