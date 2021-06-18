c74d68899e9cf67d735cd2ac45fd445b081ce37e
yangheat <yangheat@gmail.com>
2021-06-19 01:33:56 +0900

    #2.8

---
+++ useAxios/useAxios.js
@@ -0,0 +1,39 @@
+export const useAxios = (opts, axiosinstance = defaultAxios) => {
+  const [state, setState] = useState({
+    loading: true,
+    data: null,
+    error: null,
+  });
+
+  const [trigger, setTrigger] = useState(0);
+
+  const refetch = () => {
+    setState({
+      ...state,
+      loading: true,
+    });
+    setTrigger(Date.now());
+  };
+
+  useEffect(() => {
+    axiosinstance(opts)
+      .then((data) => {
+        setState({
+          ...state,
+          loading: false,
+          data,
+        });
+      })
+      .catch((error) => {
+        setState({ ...state, loading: false, error });
+      });
+  }, [trigger]);
+
+  if (!opts.url) {
+    return;
+  }
+
+  return { ...state, refetch };
+};
+
+export default useAxios;
